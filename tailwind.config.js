9506c1bede66ec236195cf2a693aa949d7026dd6
nguyenethan01 <nguyenethan01@gmail.com>
2025-04-06 21:45:52 -0700

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
